41562b0d4c560b4318b6c473835b818da9588a2c
VladMungin <vlad.mungin@yandex.ru>
2025-05-28 19:00:33 +0300

    add

--- src/page/tasks/all-tasks/all-tasks.tsx
+++ src/page/tasks/all-tasks/all-tasks.tsx
@@ -1,19 +1,21 @@
 'use client';
 
-import { Task, useGetTasks } from '@/entities/task';
-import { userStore } from '@/entities/user';
+import { Task, TASK_STATUS, useGetTasks } from '@/entities/task';
+import { adminStore, userStore } from '@/entities/user';
 import { useAtomValue } from 'jotai';
 import { MantineReactTable, MRT_ColumnDef } from 'mantine-react-table';
 import { MRT_Localization_RU } from 'mantine-react-table/locales/ru';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useMemo } from 'react';
 
 export const AllTasks = () => {
 	const user = useAtomValue(userStore);
+	const adminId = useAtomValue(adminStore);
 	const router = useRouter();
 
-	const { data: tasksData, isLoading } = useGetTasks(user?.id as string, {
-		enabled: !!user?.id,
+	const { data: tasksData, isLoading } = useGetTasks(adminId as string, {
+		enabled: !!adminId,
 	});
 
 	const columns = useMemo<MRT_ColumnDef<Task>[]>(
@@ -22,6 +24,19 @@ export const AllTasks = () => {
 				accessorKey: 'id',
 				header: '№',
 				mantineEditTextFieldProps: ({ row }) => console.log(row),
+				Cell: ({ row }) => {
+					return (
+						<Link
+							href={`/tasks/task/${row.original.id}`}
+							className='underline text-(--mantine-color-blue-2) text-lg mx-auto pl-4'
+						>
+							{row.original.id}
+						</Link>
+					);
+				},
+				size: 10,
+				maxSize: 10,
+				minSize: 10,
 			},
 			{
 				accessorKey: 'type.name',
@@ -29,17 +44,32 @@ export const AllTasks = () => {
 			},
 			{
 				accessorKey: 'title',
-				header: 'Заголовок',
-				mantineEditTextInputProps: {},
+				header: 'Название',
 			},
 			{
 				accessorKey: 'status',
 				header: 'Статус',
+				Cell: ({ row }) => {
+					console.log(row);
+					return (
+						<p>
+							{
+								TASK_STATUS[
+									row.original?.status as unknown as keyof typeof TASK_STATUS
+								]
+							}
+						</p>
+					);
+				},
 			},
 
 			{
 				accessorKey: 'employee.fullName',
 				header: 'Исполнитель',
+			},
+			{
+				accessorKey: 'createdBy.name',
+				header: 'Автор задачи',
 			},
 		],
 		[]
@@ -57,11 +87,11 @@ export const AllTasks = () => {
 					isLoading,
 				}}
 				localization={MRT_Localization_RU}
-				mantineTableBodyRowProps={({ row }) => ({
-					onClick: () => {
-						router.push(`/tasks/task/${row.original.id}`);
-					},
-				})}
+				// mantineTableBodyRowProps={({ row }) => ({
+				// 	onClick: () => {
+				// 		router.push(`/tasks/task/${row.original.id}`);
+				// 	},
+				// })}
 			/>
 		</div>
 	);
