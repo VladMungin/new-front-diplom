196904415fd181cc91ee7c608aed2aaa22a4b440
VladMungin <vlad.mungin@yandex.ru>
2025-06-09 12:13:56 +0300

    add

--- src/page/tasks/task/[slug].tsx
+++ src/page/tasks/task/[slug].tsx
@@ -11,7 +11,7 @@ import {
 } from '@/entities/task';
 import { ReassignTaskModal } from '@/entities/task/ui/reassign-task-modal/reassign-task-modal';
 import { userStore } from '@/entities/user';
-import { Button, ButtonGroup, Title } from '@mantine/core';
+import { Box, Button, ButtonGroup, LoadingOverlay, Title } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { useAtomValue } from 'jotai';
 import { useParams } from 'next/navigation';
@@ -21,14 +21,17 @@ import { useStopwatch } from 'react-timer-hook';
 export const TaskPage = () => {
 	const params = useParams();
 	const taskId = params.slug;
-	const { data: taskData, refetch } = useGetTaskById(taskId as string, {
+	const {
+		data: taskData,
+		refetch,
+		isFetched,
+	} = useGetTaskById(taskId as string, {
 		enabled: !!taskId,
 	});
 
 	const { mutateAsync: updateTaskTime } = useUpdateTaskTime(taskId as string);
-	const { mutateAsync: updateTaskStatus } = useUpdateTaskStatus(
-		taskId as string
-	);
+	const { mutateAsync: updateTaskStatus, isPending: isPendingStatus } =
+		useUpdateTaskStatus(taskId as string);
 	const { mutateAsync: updateTaskEmployee, isPending } = useUpdateTaskEmployee(
 		taskId as string
 	);
@@ -55,8 +58,20 @@ export const TaskPage = () => {
 
 	const [opened, { open, close }] = useDisclosure(false);
 
-	if (!taskData) {
+	if (!taskData && isFetched) {
 		return <div>Задача не найдена</div>;
+	}
+
+	if (!isFetched) {
+		return (
+			<Box pos='relative' mih='100vh'>
+				<LoadingOverlay
+					visible={!isFetched}
+					zIndex={1000}
+					overlayProps={{ radius: 'sm', blur: 2 }}
+				/>
+			</Box>
+		);
 	}
 
 	const formatHoursMinutes = (milliseconds: number) => {
@@ -74,144 +89,146 @@ export const TaskPage = () => {
 	const formatTime = (time: number) => {
 		return time.toString().padStart(2, '0');
 	};
-
-	return (
-		<>
-			<div className='min-h-screen bg-gray-800 font-sans'>
-				<div className='bg-gray-700 border-b border-gray-400 shadow-sm'>
-					<div className='max-w-7xl mx-auto px-4 py-4'>
-						<h1 className='text-2xl font-bold text-white'>
-							#{taskData.id} {taskData.title}
-						</h1>
-						<div className='flex gap-2 text-sm text-white'>
-							<span>
-								Создана от {new Date(taskData.createdAt).toLocaleDateString()}
-							</span>
-							<span>Создал {taskData.createdBy.name}</span>
-						</div>
-						<ButtonGroup className='mt-2'>
-							<Button
-								variant='default'
-								onClick={async () => {
-									await updateTaskStatus({
-										data: {
-											status: getNextStatus(taskData.status),
-										},
-									});
-									await refetch();
-								}}
-								disabled={taskData.employeeId !== user?.id}
-							>
-								{getTextByStatus(taskData.status)}
-							</Button>
-							<Button variant='default' onClick={open}>
-								Перевести задачу
-							</Button>
-						</ButtonGroup>
-					</div>
-				</div>
-				<div className='max-w-7xl mx-auto  py-6'>
-					<div className='bg-gray-700 border-x-0 border-y border-gray-400 shadow-sm p-6'>
-						<div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-							<div>
-								<h2 className='text-lg font-semibold text-white border-b  pb-2 mb-4'>
-									О задаче
-								</h2>
-								<div className='space-y-3 text-sm text-white'>
-									<div className='flex'>
-										<span className='font-medium w-1/3'>Исполнитель:</span>
-										<span>{taskData.employee?.fullName}</span>
-									</div>
-									<div className='flex'>
-										<span className='font-medium w-1/3'>Статус:</span>
-										<span>
-											{TASK_STATUS[
-												taskData?.status as unknown as keyof typeof TASK_STATUS
-											] || 'N/A'}
-										</span>
-									</div>
-									<div className='flex'>
-										<span className='font-medium w-1/3'>Проект:</span>
-										<span>{taskData.project?.name || 'N/A'}</span>
-									</div>
-									<div className='flex'>
-										<span className='font-medium w-1/3'>Тип:</span>
-										<span>{taskData.type.name}</span>
-									</div>
-									<div className='flex'>
-										<span className='font-medium w-1/3'>Специализация:</span>
-										<span>{taskData.specialization?.name || 'N/A'}</span>
-									</div>
-									<div className='flex'>
-										<span className='font-medium w-1/3'>
-											Время на выполнение:
-										</span>
-										<span>{timeToCompleatFormatted}</span>
-									</div>
-									<div className='flex'>
-										<span className='font-medium w-1/3'>
-											Затраченное время:
-										</span>
-										<span>{currentTimeFormatted}</span>
-									</div>
-								</div>
+	if (taskData)
+		return (
+			<>
+				<div className='min-h-screen bg-gray-800 font-sans'>
+					<div className='bg-gray-700 border-b border-gray-400 shadow-sm'>
+						<div className='max-w-7xl mx-auto px-4 py-4'>
+							<h1 className='text-2xl font-bold text-white'>
+								#{taskData.id} {taskData.title}
+							</h1>
+							<div className='flex gap-2 text-sm text-white'>
+								<span>
+									Создана от {new Date(taskData.createdAt).toLocaleDateString()}
+								</span>
+								<span>Создал {taskData.createdBy.name}</span>
 							</div>
-							<div>
-								<h2 className='text-lg font-semibold text-white border-b border-gray-400 pb-2 mb-4'>
-									Описание
-								</h2>
-								<div className='text-sm text-white whitespace-pre-wrap'>
-									{taskData.description || 'No description provided.'}
-								</div>
-							</div>
-						</div>
-					</div>
-					{taskData.status === 'IN_WORK' && (
-						<div className='mt-5 flex flex-col items-center bg-gray-700 border-y border-gray-400 shadow-sm py-2'>
-							<h1>Таймер</h1>
-							<div style={{ fontSize: '100px' }}>
-								<span>{formatTime(hours)}</span>:
-								<span>{formatTime(minutes)}</span>:
-								<span>{formatTime(seconds)}</span>
-							</div>
-							<Button
-								variant='filled'
-								onClick={() => {
-									if (isRunning) {
-										pause();
-										updateTaskTime(
-											{
-												data: {
-													currentTime: totalMilliseconds + taskData.currentTime,
-												},
+							<ButtonGroup className='mt-2'>
+								<Button
+									variant='default'
+									loading={isPendingStatus}
+									onClick={async () => {
+										await updateTaskStatus({
+											data: {
+												status: getNextStatus(taskData.status),
 											},
-											{
-												onSuccess: () => {
-													refetch();
-												},
-											}
-										);
-									} else start();
-								}}
-								className='duration-300 !rounded-3xl'
-								size='lg'
-							>
-								<Title order={3} className='!my-5'>
-									{isRunning ? 'Пауза' : 'Начать'}
-								</Title>
-							</Button>
+										});
+										await refetch();
+									}}
+									disabled={taskData.employeeId !== user?.id}
+								>
+									{getTextByStatus(taskData.status)}
+								</Button>
+								<Button variant='default' onClick={open}>
+									Перевести задачу
+								</Button>
+							</ButtonGroup>
 						</div>
-					)}
+					</div>
+					<div className='max-w-7xl mx-auto  py-6'>
+						<div className='bg-gray-700 border-x-0 border-y border-gray-400 shadow-sm p-6'>
+							<div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+								<div>
+									<h2 className='text-lg font-semibold text-white border-b  pb-2 mb-4'>
+										О задаче
+									</h2>
+									<div className='space-y-3 text-sm text-white'>
+										<div className='flex'>
+											<span className='font-medium w-1/3'>Исполнитель:</span>
+											<span>{taskData.employee?.fullName}</span>
+										</div>
+										<div className='flex'>
+											<span className='font-medium w-1/3'>Статус:</span>
+											<span>
+												{TASK_STATUS[
+													taskData?.status as unknown as keyof typeof TASK_STATUS
+												] || 'N/A'}
+											</span>
+										</div>
+										<div className='flex'>
+											<span className='font-medium w-1/3'>Проект:</span>
+											<span>{taskData.project?.name || 'N/A'}</span>
+										</div>
+										<div className='flex'>
+											<span className='font-medium w-1/3'>Тип:</span>
+											<span>{taskData.type.name}</span>
+										</div>
+										<div className='flex'>
+											<span className='font-medium w-1/3'>Специализация:</span>
+											<span>{taskData.specialization?.name || 'N/A'}</span>
+										</div>
+										<div className='flex'>
+											<span className='font-medium w-1/3'>
+												Время на выполнение:
+											</span>
+											<span>{timeToCompleatFormatted}</span>
+										</div>
+										<div className='flex'>
+											<span className='font-medium w-1/3'>
+												Затраченное время:
+											</span>
+											<span>{currentTimeFormatted}</span>
+										</div>
+									</div>
+								</div>
+								<div>
+									<h2 className='text-lg font-semibold text-white border-b border-gray-400 pb-2 mb-4'>
+										Описание
+									</h2>
+									<div className='text-sm text-white whitespace-pre-wrap'>
+										{taskData.description || 'No description provided.'}
+									</div>
+								</div>
+							</div>
+						</div>
+						{taskData.status === 'IN_WORK' && (
+							<div className='mt-5 flex flex-col items-center bg-gray-700 border-y border-gray-400 shadow-sm py-2'>
+								<h1>Таймер</h1>
+								<div style={{ fontSize: '100px' }}>
+									<span>{formatTime(hours)}</span>:
+									<span>{formatTime(minutes)}</span>:
+									<span>{formatTime(seconds)}</span>
+								</div>
+								<Button
+									variant='filled'
+									onClick={() => {
+										if (isRunning) {
+											pause();
+											updateTaskTime(
+												{
+													data: {
+														currentTime:
+															totalMilliseconds + taskData.currentTime,
+													},
+												},
+												{
+													onSuccess: () => {
+														refetch();
+													},
+												}
+											);
+										} else start();
+									}}
+									className='duration-300 !rounded-3xl'
+									size='lg'
+								>
+									<Title order={3} className='!my-5'>
+										{isRunning ? 'Пауза' : 'Начать'}
+									</Title>
+								</Button>
+							</div>
+						)}
+					</div>
 				</div>
-			</div>
-			<ReassignTaskModal
-				close={close}
-				opened={opened}
-				mutateAsync={updateTaskEmployee}
-				taskData={taskData}
-				isLoading={isPending}
-				refetch={refetch}
-			/>
-		</>
-	);
+				<ReassignTaskModal
+					close={close}
+					opened={opened}
+					mutateAsync={updateTaskEmployee}
+					taskData={taskData}
+					isLoading={isPending}
+					refetch={refetch}
+				/>
+			</>
+		);
 };
